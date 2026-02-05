38104f598ac75ed2bd6c1d170a9ffc9a39cd4262
Rafy Tho <rafytho30@gmail.com>
2026-02-05 20:45:21 +0700

    feat: add Footer component and integrate into Home page
    - Create new Footer component with newsletter signup, links, and social media icons
    - Add Footer to Home page layout below existing components
    - Remove unused import from AppContextProvider
    - Create placeholder Banner component for future use

--- client/src/contexts/AppContextProvider.jsx
+++ client/src/contexts/AppContextProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from 'react';
+import { createContext } from 'react';
 import { doctors } from '../assets/assets_frontend/assets.js';
 
 export const AppContext = createContext();
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SpacialityMenu from '../components/SpacialityMenu';
 import ToggleMode from '../components/ToggleMode';
@@ -9,6 +10,7 @@ function Home() {
       <Header />
       <SpacialityMenu />
       <TopDoctors />
+      <Footer />
     </div>
   );
 }
